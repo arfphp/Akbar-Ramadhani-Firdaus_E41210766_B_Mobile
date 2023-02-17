78d85efc2faf649cb95c9667904d9afbc7fafa57
arfphp <andre.ariaz26@gmail.com>
2023-02-17 14:52:00 +0700

    Fixed Bug

--- Minggu_2/app/src/main/java/com/example/belajarlayouting/MainActivity.java
+++ Minggu_2/app/src/main/java/com/example/belajarlayouting/MainActivity.java
@@ -46,4 +46,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, HorizontalScrollViewLayout.class);
         startActivity(intent);
     }
+
 }

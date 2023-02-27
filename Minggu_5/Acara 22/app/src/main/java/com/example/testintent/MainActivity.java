3b6bfc28371db167579b6fa346190953943e698d
arfphp <andre.ariaz26@gmail.com>
2023-02-27 11:45:14 +0700

    Bug Fixed

---
+++ Minggu_5/Acara 22/app/src/main/java/com/example/testintent/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=editText.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}

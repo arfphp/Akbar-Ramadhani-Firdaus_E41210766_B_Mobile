2badfcc46d911acd31d12e8d8a3a85178c6d0078
arfphp <andre.ariaz26@gmail.com>
2023-03-21 13:20:42 +0700

    Bug Fixed

--- Minggu_7/SharedPreferences/app/src/main/java/com/example/sharedpreferencess/RegisterActivity.java
+++ Minggu_7/SharedPreferences/app/src/main/java/com/example/sharedpreferencess/RegisterActivity.java
@@ -26,12 +26,20 @@ RegisterActivity extends AppCompatActivity {
         pass2 = findViewById(R.id.et_passwordSignup2);
         btn = findViewById(R.id.button_SignupSignup);
         btn.setOnClickListener(v -> {
+//            Memeriksa apakah nilai dari EditText "pass" sama dengan nilai dari EditText "pass2".
             if(pass.getText().toString().equals(pass2.getText().toString())){
+//                berfungsi untuk membuat sebuah objek Shared Preferences dengan nama "daftar".
+//                Context.MODE_PRIVATE adalah mode pengaksesan file Shared Preferences,
+//                dimana hanya aplikasi yang sama yang dapat mengakses data yang disimpan dalam file ini.
                 SharedPreferences sp = getSharedPreferences("daftar", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sp.edit();
+//                Menyimpan nilai dari EditText "username" ke dalam objek Shared Preferences dengan key "username".
                 editor.putString("username", username.getText().toString());
+//                Menyimpan nilai dari EditText "pass" ke dalam objek Shared Preferences dengan key "pass".
                 editor.putString("pass", pass.getText().toString());
+//                Menyimpan perubahan yang dilakukan pada objek Shared Preferences.
                 editor.commit();
+//                Menyimpan perubahan yang dilakukan pada objek Shared Preferences secara asinkron.
                 editor.apply();
                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
